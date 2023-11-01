1b06cba43f5991c6177ddbb7af8d597b66d8b7f7
Gbenga <gbengaoluwadahunsi01@gmail.com>
2023-11-02 00:09:58 +0800

    Adjusted the MyWorks conponent page heading in mobile view

--- src/Components/My-works.jsx
+++ src/Components/My-works.jsx
@@ -27,7 +27,7 @@ const Myworks = () => {
     < div className = "bg-slate-200">
     <Navbar className />
      <main className='  lg:-mt-6 lg:px-16 py-10 lg:py-10'>
-       <h2 className='text-customBlue font-extrabold md:text-xl lg:text-3xl text-center'>Selection of the works I&apos;ve previously completed.</h2> 
+       <h2 className='text-customBlue font-extrabold text-md md:text-xl lg:text-3xl text-center'>Selection of the works I&apos;ve previously completed.</h2> 
        <div className='mt-6 grid  grid-rows-1  px-6 lg:grid-cols-3 gap-6 lg:gap-8 '>
         {Cadd}  
         </div>  
