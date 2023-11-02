dd32aeac308dda0b7e8c803b3a8d2dcc5c38fc04
Gbenga <gbengaoluwadahunsi01@gmail.com>
2023-11-02 09:15:09 +0800

    fixing footer

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,8 +1,36 @@
 
  
 
+import {
+  Text,
+  Font,
+  Page,
+  View,
+  Image,
+  Document,
+  StyleSheet,
+} from '@react-pdf/renderer';
 
+// import Header from './Header';
+// import Skills from './Skills';
+// import Education from './Education';
+// import Experience from './Experience';
 import Navbar from './Navbar';
+
+
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
 
 const Resume = () =>  {
    
@@ -10,8 +38,17 @@ const Resume = () =>  {
 		<div className='bg-slate-200 h-screen'> 
       <Navbar />
       <h2 className='text-customBlue font-extrabold  md:text-xl lg:text-3xl text-center '>View my Résumé</h2>  
-
-					
+      <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+      
 		</div>
       
 	
@@ -20,3 +57,6 @@ const Resume = () =>  {
 
 
 export default Resume
+
+
+
